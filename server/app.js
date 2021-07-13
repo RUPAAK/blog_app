8fd66d3a6abef4b314de56ee726f8186f54e81ea
Rupak <rupakt525@gmail.com>
2021-07-13 15:11:31 +0545

    Initial structure set

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express= require('express')
+const mongoose= require('mongoose');
+const dotenv= require('dotenv')
+const cors= require('cors')
+
+const app= express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO, {
+    useUnifiedTopology: true, useNewUrlParser: true
+}).then(()=>console.log('CONNECITON TO DATABASE'))
+.catch((e)=>{
+    console.error(`Error-${e.message}`)
+    process.exit(1)
+})
+
+
+app.listen(process.env.PORT || 5000, ()=> console.log('SERVER UP AND RUNNING'))
